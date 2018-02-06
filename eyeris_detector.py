bfeb639fdfb9fbb3300bc4a60b9e463da094fa4e
wuf28 <36114051+wuf28@users.noreply.github.com>
2018-02-06 16:32:42 -0500

    Add files via upload

--- eyeris_detector.py
+++ eyeris_detector.py
@@ -2,7 +2,7 @@ import cv2
 import numpy
 from os.path import join
 
-
+f = open("lc1.txt","w+")
 def show_image_with_data(frame, blinks, irises, err=None):
     """
     Helper function to draw points on eyes and display frame
@@ -12,6 +12,8 @@ def show_image_with_data(frame, blinks, irises, err=None):
     :param err: for displaying current error in Lucas-Kanade tracker
     :return:
     """
+ 
+ 
     font = cv2.FONT_HERSHEY_SIMPLEX
     if err:
         cv2.putText(frame, str(err), (20, 450), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
@@ -27,10 +29,13 @@ class ImageSource:
     """
     def __init__(self):
         self.capture = cv2.VideoCapture(0)
+        #self.capture = cv2.imread("right2.png")
+        
 
     def get_current_frame(self, gray=False):
         ret, frame = self.capture.read()
         frame = cv2.flip(frame, 1)
+        #frame = cv2.imread("right2.png")
         if not gray:
             return frame
         return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -43,6 +48,8 @@ class CascadeClassifier:
     """
     This classifier is trained by default in OpenCV
     """
+    
+
     def __init__(self, glasses=True):
         if glasses:
             self.eye_cascade = cv2.CascadeClassifier(join('haar', 'haarcascade_eye_tree_eyeglasses.xml'))
@@ -52,12 +59,21 @@ class CascadeClassifier:
     def get_irises_location(self, frame_gray):
         eyes = self.eye_cascade.detectMultiScale(frame_gray, 1.3, 5)  # if not empty - eyes detected
         irises = []
-
+        #base = []
+        #delta = []
+        
         for (ex, ey, ew, eh) in eyes:
+            ######################################################
+            #base.append(ew)
+            #if len(base) >=3
+            #    delta.append(base[])
+            
+            #######################################################
             iris_w = int(ex + float(ew / 2))
             iris_h = int(ey + float(eh / 2))
             irises.append([numpy.float32(iris_w), numpy.float32(iris_h)])
-
+            f.write("{} {}\r\n".format(iris_w,iris_h))
+    
         return numpy.array(irises)
 
 
@@ -98,6 +114,7 @@ class EyerisDetector:
     Algorithm used in detector is designed for one person (with two eyes)
     It can detect more than two eyes, but it tracks only two
     """
+    
     def __init__(self, image_source, classifier, tracker):
         self.tracker = tracker
         self.classifier = classifier
@@ -123,12 +140,13 @@ class EyerisDetector:
             show_image_with_data(frame, self.blinks, self.irises)
             k = cv2.waitKey(30) & 0xff
             old_gray = gray.copy()
-
         self.image_source.release()
         cv2.destroyAllWindows()
+
 
 
 eyeris_detector = EyerisDetector(image_source=ImageSource(), classifier=CascadeClassifier(),
                                  tracker=LucasKanadeTracker())
 eyeris_detector.run()
 
+f.close()
